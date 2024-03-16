b7c4b63f5b2e2c0a951deca60c3e34d6e40e9296
almsam <44277999+almsam@users.noreply.github.com>
2024-03-15 21:09:48 -0700

    Create AdminController.java

---
+++ spring boot/demo/src/main/java/com/firstTry/demo/AdminController.java
@@ -0,0 +1,11 @@
+package main.java.com.firstTry.demo;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AdminController {
+
+
+}
