eb1bf4ef7bfe59dab0a85940b0cbd821d315dbc2
almsam <44277999+almsam@users.noreply.github.com>
2024-03-16 19:41:59 -0700

    add spring boot proj

---
+++ mejsp/jspart/jspart/src/main/java/jspart/JspartApplication.java
@@ -0,0 +1,13 @@
+package jspart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JspartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JspartApplication.class, args);
+	}
+
+}
