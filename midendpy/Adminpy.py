3ae5a5c7190e92c3da7a3d07f7a2c7c324b618ca
almsam <44277999+almsam@users.noreply.github.com>
2024-03-16 20:53:00 -0700

    refactor

--- midendpy/Adminpy.py
+++ midendpy/Adminpy.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # Define routes
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("Admin.html")
 
 
 @app.route("/mute", methods=["POST"])
