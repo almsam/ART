a5b7d2528ac17bf04c8de4de833789767e06cb2b
almsam <44277999+almsam@users.noreply.github.com>
2024-03-16 20:08:27 -0700

    Update JspartApplication.java

--- mejsp/jspart/jspart/src/main/java/jspart/JspartApplication.java
+++ mejsp/jspart/jspart/src/main/java/jspart/JspartApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JspartApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JspartApplication.class, args);
+		// SpringApplication.run(JspartApplication.class, args);
 	}
 
 }
