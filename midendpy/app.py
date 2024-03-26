07cabef3e3da97411b843c3581bb9ace10f178d1
almsam <44277999+almsam@users.noreply.github.com>
2024-03-26 12:27:26 -0700

    skeleton

---
+++ midendpy/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+from Adminpy import admin_bp
+from Adminserverpy import admin_server_bp
+from loginpy import login_bp
+from signuppy import signup_bp
+
+app = Flask(__name__)
+app.register_blueprint(admin_bp)
+app.register_blueprint(admin_server_bp)
+app.register_blueprint(login_bp)
+app.register_blueprint(signup_bp)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
