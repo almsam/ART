b3a033512b18c27a1caaf1596ec80bcfa9a42153
almsam <44277999+almsam@users.noreply.github.com>
2024-03-26 13:38:18 -0700

    changed landing page

--- midendpy/app.py
+++ midendpy/app.py
@@ -5,16 +5,16 @@ from Adminserverpy import admin_server_bp
 from loginpy import login_bp
 from signuppy import signup_bp
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="HTML")
+app.register_blueprint(signup_bp)
 app.register_blueprint(admin_bp)
 app.register_blueprint(admin_server_bp)
 app.register_blueprint(login_bp)
-app.register_blueprint(signup_bp)
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("LogIn.html")
 
 
 if __name__ == "__main__":
