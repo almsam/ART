441f8404b18a7f75cd98d5c1089bc5cff63ff231
almsam <44277999+almsam@users.noreply.github.com>
2024-03-26 15:25:17 -0700

    added blueprint

--- midendpy/app.py
+++ midendpy/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Blueprint
 
 from Adminpy import admin_bp
 from Adminserverpy import admin_server_bp
@@ -6,15 +6,25 @@ from loginpy import login_bp
 from signuppy import signup_bp
 
 app = Flask(__name__, template_folder="HTML")
-app.register_blueprint(signup_bp)
-app.register_blueprint(admin_bp)
-app.register_blueprint(admin_server_bp)
 app.register_blueprint(login_bp)
+# app.register_blueprint(signup_bp)
+# app.register_blueprint(admin_bp)
+# app.register_blueprint(admin_server_bp)
+
+admin_bp = Blueprint("admin", __name__)
 
 
 @app.route("/")
 def index():
+    # render_template("LogIn.html")
+    # app.register_blueprint(login_bp)
     return render_template("LogIn.html")
+
+
+@app.route("/pageAdmin")
+def pageAdmin():
+    print("pageAdminRun")
+    return render_template("Admin.html")
 
 
 if __name__ == "__main__":
