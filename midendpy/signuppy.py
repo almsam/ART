ab886d36c20146a3ed54fab6d27ff16de9593b1f
almsam <44277999+almsam@users.noreply.github.com>
2024-03-25 19:54:32 -0700

    verification added

--- midendpy/signuppy.py
+++ midendpy/signuppy.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template
+import re
 
 app = Flask(__name__, template_folder="html")
 username = ""
@@ -6,6 +7,11 @@ Password = ""
 confirmPassword = ""
 email = ""
 dob = ""
+
+a = False
+b = False
+c = False
+d = False
 
 
 # Define routes
@@ -38,18 +44,44 @@ def login():
         dob,
     )
 
-    return (
-        "Received username: "
-        + username
-        + " password: "
-        + password
-        + " cp: "
-        + confirmPassword
-        + "email: "
-        + email
-        + " dob: "
-        + dob
-    )
+    print("attempting to authenticate:")
+
+    if len(username) > 8:
+        a = True
+    if password == confirmPassword:
+        b = True
+    email_regex = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+    if re.match(email_regex, email):
+        c = True
+    if int(dob[:4]) < 2005:
+        d = True
+
+    if a == False:
+        print("Username too short:", username)
+        return "username too short: " + username
+    elif b == False:
+        print("Passwords don't match:", password, ",", confirmPassword)
+        return "passwords dont match: " + password + ", " + confirmPassword
+    elif c == False:
+        print("Email invalid:", email)
+        return "email invalid: " + email
+    elif a == False:
+        print("You must be >18:", dob)
+        return "you must be >18: " + dob
+    else:
+        print("sign up success")
+        return (
+            "login success, Received username: "
+            + username
+            + " password: "
+            + password
+            + " cp: "
+            + confirmPassword
+            + "email: "
+            + email
+            + " dob: "
+            + dob
+        )
 
 
 if __name__ == "__main__":
