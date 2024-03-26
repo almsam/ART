b2cbc9adf76fde0b7fecd6e18ed657b56c4b8c1e
almsam <44277999+almsam@users.noreply.github.com>
2024-03-26 15:25:05 -0700

    authenticiation added

--- midendpy/loginpy.py
+++ midendpy/loginpy.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, request, render_template
+from flask import Blueprint, request, redirect, url_for, render_template
+from flask import current_app
 
 login_bp = Blueprint("login", __name__)
 
@@ -25,4 +26,30 @@ def login():
     print("Username:", username)
     print("Password:", password)
     # Perform authentication logic here
+
+    if authenticate(request.form["username"], request.form["password"]):
+        # Redirect to the admin page upon successful login
+        # print(render_template("admin.html"))
+        # app.pageAdmin()
+        invoke_pageAdmin()
+        return render_template("admin.html")
+        # return redirect(url_for("admin.admin_page"))
+    else:
+        # Return some response for unsuccessful login
+        return "Login failed. Please try again."
+
     return "Received username: " + username + ", password: " + password
+
+
+def authenticate(UN, PW):
+    if UN == "ss":
+        if PW == "ss":
+            return True
+    return False
+
+
+def invoke_pageAdmin():
+    with current_app.test_request_context():
+        response = current_app.test_client().get("/pageAdmin")
+        # Process the response if needed
+        return response
