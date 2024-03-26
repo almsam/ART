7e160d81ed10bbf8b4b803fd9f12faa82c9f6640
almsam <44277999+almsam@users.noreply.github.com>
2024-03-26 13:05:20 -0700

    revert comment

--- midendpy/app.py
+++ midendpy/app.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template
 
-# from Adminpy import admin_bp
-# from Adminserverpy import admin_server_bp
-# from loginpy import login_bp
+from Adminpy import admin_bp
+from Adminserverpy import admin_server_bp
+from loginpy import login_bp
 from signuppy import signup_bp
 
 app = Flask(__name__)
-# app.register_blueprint(admin_bp)
-# app.register_blueprint(admin_server_bp)
-# app.register_blueprint(login_bp)
+app.register_blueprint(admin_bp)
+app.register_blueprint(admin_server_bp)
+app.register_blueprint(login_bp)
 app.register_blueprint(signup_bp)
 
 
