950be0278ea66832bd5548bc418a39335ed916ce
Weebtrain <156955415+Weebtrain@users.noreply.github.com>
2024-03-31 16:35:58 -0700

    Update databaseConnectTemplate.py
    Functions as templates, needs tests and more template work

--- Database/databaseConnectTemplate.py
+++ Database/databaseConnectTemplate.py
@@ -1,23 +1,71 @@
 import mysql.connector #must download in pip
-try:
-    ARTdb = mysql.connector.connect(    #building connection to database
-    host="localhost",                   #database is hosted on local machine
-    user="ART",                         #Username to access database
-    password="ARTpw",                   #Password to access database
-    database='ART',                     #Database name
-    ssl_disabled='True'
-    )
 
-    cursor = ARTdb.cursor()             #builds a cursor to retain selected information from queries
-    query = "SELECT username from User" #SQL Query
-    cursor.execute(query)               #Executes query with cursor
+def noVariableSelectTemplate():
+    try:
+        ARTdb = mysql.connector.connect(    #building connection to database
+        host="localhost",                   #database is hosted on local machine
+        user="ART",                         #Username to access database
+        password="ARTpw",                   #Password to access database
+        database='ART',                     #Database name
+        ssl_disabled='True',
+        )
 
-    for (username) in cursor:           #How to iterate over cursor
-        print(username)
+        cursor = ARTdb.cursor()             #builds a cursor to retain selected information from queries
+        query = "SELECT username from User" #SQL Query
+        cursor.execute(query)               #Executes query with cursor
 
-    cursor.close()
-except mysql.connector.Error as err:
-    print(err)
-finally:
-    ARTdb.close()
+        for (username) in cursor:           #How to iterate over cursor
+            print(username)
 
+        cursor.close()
+    except mysql.connector.Error as err:
+        print(err)
+    finally:
+        ARTdb.close()
+
+def variableSelectTemplate():
+    try:
+        ARTdb = mysql.connector.connect(    #building connection to database
+        host="localhost",                   #database is hosted on local machine
+        user="ART",                         #Username to access database
+        password="ARTpw",                   #Password to access database
+        database='ART',                     #Database name
+        ssl_disabled='True',
+        )
+
+        cursor = ARTdb.cursor()             #builds a cursor to retain selected information from queries
+        query = "SELECT username from User where username='%s'" #SQL Query with variable inserts: %s for string, %d for integers
+        cursor.execute(query, ("string"))               #Executes query with cursor
+
+        for (username) in cursor:           #How to iterate over cursor
+            print(username)
+
+        cursor.close()
+    except mysql.connector.Error as err:
+        print(err)
+    finally:
+        ARTdb.close()
+
+def insertTemplate():
+    try:
+        ARTdb = mysql.connector.connect(    #building connection to database
+        host="localhost",                   #database is hosted on local machine
+        user="ART",                         #Username to access database
+        password="ARTpw",                   #Password to access database
+        database='ART',                     #Database name
+        ssl_disabled='True',
+        )
+
+        cursor = ARTdb.cursor()             #builds a cursor to retain selected information from queries
+        query = "INSERT INTO User (username, password, email) VALUES (%s, %s, %s)"   #SQL Query with variable inserts: %s for string, %d for integers
+        cursor.execute(query, ("string", "asd", "asd@asd.ca"))               #Executes query with cursor
+        ARTdb.commit()      #uploads and commits data to database
+
+        cursor.close()
+    except mysql.connector.Error as err:
+        print(err)
+    finally:
+        ARTdb.close()
+
+insertTemplate()
+noVariableSelectTemplate()
